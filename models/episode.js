a01f7fb0ef1e4c201b4a2d9b6ac8e8f7cad49c75
KristenLeach <31492991+KristenLeach@users.noreply.github.com>
2018-11-12 15:33:01 -0800

    Completed mongoose schema for Episode

--- models/episode.js
+++ models/episode.js
@@ -18,7 +18,38 @@ const Episode = mongoose.model('Episode', {
         }
     ],
     description: [
-        
-    ]
+        {
+            text: {
+                type: String,
+                required: false,
+                minlength: 1,
+                trim: true
+            }
+        }
+    ],
+    show: [
+        {
+            text: {
+                type: String,
+                required: true,
+                minlength: 1,
+                trim: true
+            }
+        }
+    ], 
+    latlong: {
+        lat: {
+            type: Number,
+            required: true,
+            minlength: 1,
+            trim: true
+        }, 
+        lng: {
+            type: Number,
+            required: true,
+            minlength: 1,
+            trim: true
+        }
+    }
 
 })
