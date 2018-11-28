6769819c0fee69551fc2a65311031f5053a87690
KristenLeach <31492991+KristenLeach@users.noreply.github.com>
2018-11-28 12:22:38 -0800

    Added CORS for cross browser requests

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const express = require('express');
-const bodyParser = require('body-parser');
+const cors = require('cors');
 const mongoose = require('mongoose');
 
 const {Episode} = require('./models/episode');
@@ -9,7 +9,7 @@ mongoose.connect(`mongodb://${process.env.USERNAME}:${process.env.PASSWORD}${pro
 
 const app = express();
 //middleware to parse JSON
-app.use(bodyParser.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
     Episode.find().then((episodes) => {
